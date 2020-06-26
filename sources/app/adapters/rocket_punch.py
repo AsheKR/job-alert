ac58375d0613273932e8e3613b384b3c466dc87b
Gil <sgy@tpay.co.kr>
2020-06-26 13:58:26 +0900

    [Add] contents of the Adapter depend on the Parser

--- sources/app/adapters/rocket_punch.py
+++ sources/app/adapters/rocket_punch.py
@@ -4,9 +4,11 @@ from schemas.company import CompanySchema
 
 
 class RocketPunchAdapter:
-    def __init__(self, data: Union[List, dict]):
+    def __init__(self, data: Union[List, dict], parser):
         self._data = data
         self._aware_data = None
+
+        self._parser = parser
 
     @property
     def aware_data(self) -> Union[List, dict]:
@@ -19,3 +21,15 @@ class RocketPunchAdapter:
             self._aware_data = schema.load(self._data)
 
         return self._aware_data
+
+    def to_html(self) -> str:
+        parser = self._parser
+        html = ''
+
+        if isinstance(self._data, List):
+            for company in self.aware_data:
+                html += parser.to_html(company)
+        else:
+            html += parser.to_html(self._data)
+
+        return html
