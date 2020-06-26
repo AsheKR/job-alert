f919644bc76f61f20635ce06b90493683173f30f
Gil <sgy@tpay.co.kr>
2020-06-26 11:14:07 +0900

    [Add] RocketPunchAdapter

---
+++ sources/app/adapters/rocket_punch.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from pprint import pprint
+from typing import Union, List
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from crawlers.rocket_punch import RocketPunchCrawler
+from schemas.company import CompanySchema
+
+
+class RocketPunchAdapter:
+    def __init__(self, data: Union[List, dict]):
+        self._data = data
+        self._aware_data = None
+
+    @property
+    def aware_data(self) -> Union[List, dict]:
+        if not self._aware_data:
+            if isinstance(self._data, List):
+                schema = CompanySchema(many=True)
+            else:
+                schema = CompanySchema()
+
+            self._aware_data = schema.load(self._data)
+            pprint(self._aware_data)
+
+        return self._aware_data
