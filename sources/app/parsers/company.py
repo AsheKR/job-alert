8d8bffd8e14e79adb1105c72e1c433806f698a25
Gil <sgy@tpay.co.kr>
2020-06-29 15:31:29 +0900

    [Drop] Remove Company Parser

--- sources/app/parsers/company.py
+++
@@ -1,4 +0,0 @@
-class CompanyParser:
-    @staticmethod
-    def to_html(data: dict):
-        return f'<h1>{data.get("name")}</h1>'
--- sources/app/parsers/job_detail.py
+++
@@ -1,4 +0,0 @@
-class JobDetailParser:
-    @staticmethod
-    def to_html(data: dict):
-        return f'<h3>{data.get("title")}</h3>'
