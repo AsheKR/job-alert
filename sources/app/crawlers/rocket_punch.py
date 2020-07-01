e0be66a878e3b53f398b7e9f7f2f6383d368b6da
Gil <sgy@tpay.co.kr>
2020-07-01 15:57:34 +0900

    [Refactor] Change RocketPunchCrawler to inhret BaseCrawler

--- sources/app/crawlers/rocket_punch.py
+++ sources/app/crawlers/rocket_punch.py
@@ -5,44 +5,21 @@ import requests
 from bs4 import BeautifulSoup, Tag
 from requests import Response
 
+from crawlers import BaseCrawler
 
-class RocketPunchCrawler:
+
+class RocketPunchCrawler(BaseCrawler):
     BASE_URL = 'https://www.rocketpunch.com'
-    BASE_JOB_URL = BASE_URL + '/api/jobs/template'
+    SEARCH_PATH = '/api/jobs/template'
+
     BASE_TARGET_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'targets')
-    TARGET_FILE = 'latest_rocket_punch_id.txt'
+    TARGET_FILE_NAME = 'latest_rocket_punch_id.txt'
 
-    def __init__(self, keywords: List[str] = None):
-        # inner Controlled Data
-        self._file_data = []
-        self._companies = []
-        self._keywords = []
+    # 기본 제공할 query_string 이 존재하는 경우 지정한다.
+    DEFAULT_QUERY_STRING: List[tuple] = [('sort', 'recent')]
+    KEYWORD_NAME: str = 'keywords'
 
-        if keywords is None:
-            keywords = []
-
-        self.query_string = [('sort', 'recent')]
-        self.extend_keywords(keywords)
-
-    @property
-    def keywords(self) -> List[str]:
-        return self._keywords
-
-    @property
-    def file_data(self) -> List[str]:
-        if not self._file_data:
-            target_path = os.path.join(self.BASE_TARGET_PATH, self.TARGET_FILE)
-
-            if not os.path.exists(target_path):
-                return []
-
-            with open(target_path, 'r') as target_file:
-                self._file_data = target_file.read().split('\n')
-
-        return self._file_data
-
-    @property
-    def companies(self) -> List[dict]:
+    def get_companies(self) -> List[dict]:
         if not self._companies:
             response = self.get_response()
             html = self.parse_html_from_response(response)
@@ -50,77 +27,16 @@ class RocketPunchCrawler:
 
         return self._companies
 
-    def extend_keywords(self, keywords: List[str]) -> None:
-        self._keywords.extend(keywords)
-        self.query_string.extend([('keywords', keyword) for keyword in keywords])
-
-    def get_new_companies(self) -> List[dict]:
-        latest_company_id_from_file = self.get_latest_company_id_from_file()
-        latest_company_id_from_api = self.get_latest_company_id_from_api()
-
-        # 첫 크롤링 시
-        if not latest_company_id_from_file:
-            return []
-
-        # 업데이트가 없을 시
-        if latest_company_id_from_file == latest_company_id_from_api:
-            return []
-
-        new_companies = []
-
-        for company in self.companies:
-            # ID 가 동일한 것이 있을 때까지 신규 등록 공고.
-            if company.get('id') == latest_company_id_from_file:
-                break
-            new_companies.append(company)
-
-        return new_companies
-
-    def write_latest_company_id_to_file(self) -> None:
-        latest_company_id = self.get_latest_company_id_from_api()
-        target_path = os.path.join(self.BASE_TARGET_PATH, self.TARGET_FILE)
-        target_string = f'{",".join(self.keywords)}:{latest_company_id}'
-
-        file_data = self.file_data.copy()
-
-        for index, line in enumerate(file_data):
-            if line.startswith(','.join(self.keywords)):
-                file_data[index] = target_string
-                break
-        else:
-            file_data.append(target_string)
-
-        with open(target_path, 'w') as target_file:
-            target_file.write('\n'.join(file_data))
-
-    def get_latest_company_id_from_file(self) -> str:
-        data = self.file_data
-
-        for line in data:
-            if line.startswith(','.join(self.keywords)):
-                latest_company_id = line.split(':')[1]
-                break
-        else:
-            latest_company_id = ''
-
-        return latest_company_id
-
-    def get_latest_company_id_from_api(self) -> str:
-        if not self.companies:
-            raise ValueError(
-                f'크롤러로 가져온 결과가 존재하지 않습니다.'
-            )
-
-        return self.companies[0].get('id')
-
     def get_response(self) -> Response:
-        response = requests.get(self.BASE_JOB_URL, params=self.query_string)
+        response = requests.get(self.BASE_URL + self.SEARCH_PATH, params=self.query_string)
 
         if not response.status_code == 200:
             raise ValueError(
                 f'결과를 가져오는데 실패했습니다.\n'
                 f'StatusCode: {response.status_code}\n'
-                f'response: {response.raw}'
+                f'response: {response.raw}\n'
+                f'url: {self.BASE_URL}\n'
+                f'params: {self.query_string}'
             )
 
         return response
@@ -188,8 +104,10 @@ class RocketPunchCrawler:
         job_stats_info = getattr(soup_job_detail.select_one('.job-stats-info'), 'text', '')
 
         # TODO: Classification meta tag
-        job_detail_date_meta_1 = getattr(soup_job_detail.select_one('.job-dates > span:nth-child(1)'), 'text', '').strip()
-        job_detail_date_meta_2 = getattr(soup_job_detail.select_one('.job-dates > span:nth-child(2)'), 'text', '').strip()
+        job_detail_date_meta_1 = getattr(soup_job_detail.select_one('.job-dates > span:nth-child(1)'), 'text',
+                                         '').strip()
+        job_detail_date_meta_2 = getattr(soup_job_detail.select_one('.job-dates > span:nth-child(2)'), 'text',
+                                         '').strip()
 
         return {
             'id': job_detail_id,
