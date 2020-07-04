6c62c4951926b87c5f88413b5a7f1d779696cc4a
Gil <sgy@tpay.co.kr>
2020-07-04 16:30:33 +0900

    [Reword] Base 의 메서드 주석 추가

--- sources/app/parsers/__init__.py
+++ sources/app/parsers/__init__.py
@@ -4,4 +4,7 @@ from typing import List
 class BaseParser:
     @staticmethod
     def parse(results: List[dict], **kwargs):
+        """
+        results 를 가지고 원하는 형식으로 변경하여 리턴해주세요
+        """
         raise NotImplementedError('BaseParser 를 상속하면 parse 메서드를 구현해야합니다.')
--- sources/app/senders/__init__.py
+++ sources/app/senders/__init__.py
@@ -19,7 +19,14 @@ class BaseSender:
         return self._content
 
     def prepare_data(self, results: List[dict], **kwargs):
+        """
+        kwargs 에서는 self.result_parser 에서 필요한 추가 데이터를 받을 수 있도록 합니다.
+        self.result_parser 를 사용하여 self._content 에 대입하는 코드를 작성해주세요.
+        """
         raise NotImplementedError('BaseSender 는 prepare_data 를 반드시 구현해야합니다.')
 
     def send(self):
+        """
+        self._content 를 사용하여 전송해주세요.
+        """
         raise NotImplementedError('BaseSender 는 send 메서드를 반드시 구현해야합니다.')
